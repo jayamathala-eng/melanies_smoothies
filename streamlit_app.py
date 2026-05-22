0f362dfc4996bba52714aa1da92c2318e2f4782b
jayamathala-eng <jaya.mathala@gmail.com>
2026-05-22 14:36:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ elif ingredients_list:
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 if smoothiefroot_response.status_code == 200:
     #st.json(smoothiefroot_response.json())
-    st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
+    st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 else:
     st.error("Could not fetch fruit details from API.")
